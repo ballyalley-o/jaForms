4795f5b83938df168e985fecb6eb788df178cc44
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-02-03 08:23:26 +1300

    funcs

---
+++ app.js
@@ -0,0 +1,31 @@
+document.getElementById('first-name').addEventListener('blur', validateName)
+document.getElementById('last-name').addEventListener('blur', validateLastName)
+document.getElementById('email').addEventListener('blur', validateEMail)
+document.getElementById('username').addEventListener('blur', validateUserName)
+document.getElementById('password').addEventListener('blur', validatePassword)
+document.getElementById('address').addEventListener('blur', validateAddress)
+
+
+function validateName() {
+
+}
+
+function validateLastName() {
+
+}
+
+function validateEMail() {
+
+}
+
+function validateUserName() {
+
+}
+
+function validatePassword() {
+
+}
+
+function validateAddress() {
+
+}
